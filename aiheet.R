001d4b2197316b1696171549ac529b71d4408651
Ernvall Toni <h02281@haaga-helia.fi>
2022-03-23 14:55:13 +0200

    Luonnos käsiteltävistä aiheista.

---
+++ aiheet.R
@@ -0,0 +1,19 @@
+# Johdanto R-kieleen
+
+# Muuttujat ja niiden aritmeettiset operaatiot
+
+# Funktiot
+
+# For-silmukka ja ehtorakenne (if-else)
+
+# Taulukoiden muodostaminen ja muokkaaminen (data.frame, cbind, rbind)
+
+# Frekvenssitaulu ja ristiintaulukointi (table)
+
+# Kuvaajia (plot, hist)
+
+# Lineaarinen regressiomalli
+
+# Jakaumia
+
+# Testejä
